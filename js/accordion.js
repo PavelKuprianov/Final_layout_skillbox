9d93ca7e4689c8af6fc7aa83b6760bc01d402c16
Pavel Kuprianov <97345976+PavelKuprianov@users.noreply.github.com>
2022-05-04 23:09:34 +0300

    4.05

--- js/accordion.js
+++ js/accordion.js
@@ -1,36 +1,7 @@
-const accordionButtons = document.querySelectorAll('.accordion__btn');
-const accordionContent = document.querySelectorAll('.accordion__content');
-
-accordionButtons.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    const control = e.currentTarget;
-    const content = control.nextElementSibling;
-    console.log(content)
-
-    if (content.classList.contains('open')) {
-      content.classList.toggle('open')
-      btn.querySelector('.accordion__btn_arrow').classList.remove('open_arrow')
-      content.style.maxHeight = null;
-      control.setAttribute('aria-expanded', false);
-      content.setAttribute('aria-hidden', true);
-    } else {
-      accordionButtons.forEach((button) => {
-
-        content.classList.remove('open');
-
-        button.querySelector('.accordion__btn_arrow').classList.remove('open_arrow')
-        content.style.maxHeight = null;
-        control.setAttribute('aria-expanded', false);
-        content.setAttribute('aria-hidden', true);
-      });
-
-      control.setAttribute('aria-expanded', true);
-      content.setAttribute('aria-hidden', false);
-
-      content.classList.add('open');
-      content.style.maxHeight = content.scrollHeight + 'px';
-      control.querySelector('.accordion__btn_arrow').classList.add('open_arrow')
-
-    }
-  })
-})
+new Accordion('.accordion-container', {
+  duration: 500,
+  // elementClass: accordion,
+  // triggerClass: accordion__btn,
+  // panelClass: accordion__content,
+  // activeClass: accordion - active,
+});
